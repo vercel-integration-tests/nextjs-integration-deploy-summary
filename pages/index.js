0c53b7820705e6fa1a765fd926dccbbd81151a17
William Li <williamli@bbi.io>
2020-01-23 13:26:05 +0800

    testing deploy-summary integration

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ const Home = () => {
       <Nav />
 
       <div className="hero">
-        <h1 className="title">Welcome to Next!</h1>
+        <h1 className="title">Welcome to Next Integration Test (Deployment Summary)!</h1>
         <p className="description">
           To get started, edit the <code>pages/index.js</code> or{' '}
           <code>pages/api/date.js</code> files, then save to reload.
